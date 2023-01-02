c790ae00cfe830ee86b79ce8d58b96b7f1f19c94
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2023-01-02 09:43:30 -0300

    Corrigindo conexão com o banco de dados

--- backend/src/Models/dbconnection.js
+++ backend/src/Models/dbconnection.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const connection = new Pool({
   host: process.env.PG_HOST,
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
+  database: process.env.PG_DB,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
