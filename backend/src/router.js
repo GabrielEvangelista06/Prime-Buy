ef3edca13a05657facd76d58653cbe0c460ec4ae
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2023-01-02 12:00:55 -0300

    Adicionando middlware na rota de criação de um produto

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const productsController = require('./Controllers/ProductController');
+const productMiddleware = require('./Middlewares/ProductMiddleware');
 
 const router = express.Router();
 
 router.get('/products', productsController.getAll);
-router.post('/products', productsController.createProduct);
+router.post('/products', productMiddleware.validateBody, productsController.createProduct);
 
 module.exports = router;
