18fc78032291334b07b731017b470e9813b7cc3e
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2023-01-02 14:54:44 -0300

    Criando novos middlewares, cada middleware valida um campo

--- backend/src/Middlewares/ProductMiddleware.js
+++ backend/src/Middlewares/ProductMiddleware.js
@@ -1,21 +1,45 @@
-const validateBody = (req, res, next) => {
+const validateName = (req, res, next) => {
   const { body } = req;
 
-  if (
-    body.name === undefined ||
-    body.description === undefined ||
-    body.price === undefined
-  ) {
-    return res.status(400).json({ message: 'The fields are required' });
+  if (body.name === undefined) {
+    return res.status(400).json({ message: 'The field name are required' });
   }
 
-  if (body.name === '' || body.description === '' || body.price === '') {
-    return res.status(400).json({ message: 'The fields cannot be empty' });
+  if (body.name === '') {
+    return res.status(400).json({ message: 'Name cannot be empty' });
+  }
+
+  next();
+};
+
+const validateDescription = (req, res, next) => {
+  const { body } = req;
+
+  if (body.description === undefined) {
+    return res
+      .status(400)
+      .json({ message: 'The field description are required' });
+  }
+
+  if (body.description === '') {
+    return res.status(400).json({ message: 'Description cannot be empty' });
+  }
+
+  next();
+};
+
+const validatePrice = (req, res, next) => {
+  const { body } = req;
+
+  if (body.price === undefined) {
+    return res.status(400).json({ message: 'The field price are required' });
   }
 
   next();
 };
 
 module.exports = {
-  validateBody,
+  validateName,
+  validateDescription,
+  validatePrice,
 };
