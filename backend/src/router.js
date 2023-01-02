4f9e6c1a4149dc754d3bdd67d6ac4cf565c16d56
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2023-01-02 11:22:25 -0300

    Criando rota para a criação de novos produtos

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,5 +4,6 @@ const productsController = require('./Controllers/ProductController');
 const router = express.Router();
 
 router.get('/products', productsController.getAll);
+router.post('/products', productsController.createProduct);
 
 module.exports = router;
