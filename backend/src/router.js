31b1b6857f07bd09378ebba7564a74c6491fae16
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2023-01-02 14:45:58 -0300

    Adicionando middleware na rota de atualizar um produto para fazer a validação do corpo da requisição

--- backend/src/router.js
+++ backend/src/router.js
@@ -5,8 +5,16 @@ const productMiddleware = require('./Middlewares/ProductMiddleware');
 const router = express.Router();
 
 router.get('/products', productsController.getAll);
-router.post('/products', productMiddleware.validateBody, productsController.createProduct);
+router.post(
+  '/products',
+  productMiddleware.validateBody,
+  productsController.createProduct
+);
 router.delete('/products/:id', productsController.deleteProduct);
-router.put('/products/:id', productsController.updateProduct);
+router.put(
+  '/products/:id',
+  productMiddleware.validateBody,
+  productsController.updateProduct
+);
 
 module.exports = router;
