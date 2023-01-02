0173808c0a9c4974a0b8f4862494ee3792ef8d28
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2023-01-02 09:44:28 -0300

    Corrigindo o diretório da conexão

--- backend/src/Models/products.js
+++ backend/src/Models/products.js
@@ -1,4 +1,4 @@
-const connection = require('./connection');
+const connection = require('./dbconnection');
 
 const getAll = async () => {
   const products = await connection.query('SELECT * FROM products');
