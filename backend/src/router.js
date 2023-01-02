4a170564f607e0885e4560cb0bcffec26a17d023
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2023-01-02 14:30:36 -0300

    Criando rota para a função de deletar um produto

--- backend/src/router.js
+++ backend/src/router.js
@@ -5,6 +5,9 @@ const productMiddleware = require('./Middlewares/ProductMiddleware');
 const router = express.Router();
 
 router.get('/products', productsController.getAll);
-router.post('/products', productMiddleware.validateBody, productsController.createProduct);
+router.post(
+  '/products', productMiddleware.validateBody, productsController.createProduct
+);
+router.delete('/products/:id', productsController.deleteProduct);
 
 module.exports = router;
