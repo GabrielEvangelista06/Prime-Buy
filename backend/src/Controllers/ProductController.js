6dff873f9d6ec29f3b8319414a5bb8cafbed1c07
Gabriel Evangelista dos Santos <68117644+GabrielEvangelista06@users.noreply.github.com>
2023-01-02 11:19:42 -0300

    Criando função para a criação de um produto

--- backend/src/Controllers/ProductController.js
+++ backend/src/Controllers/ProductController.js
@@ -1,11 +1,18 @@
 const productsModel = require('../Models/products');
 
-const getAll = async (req, res) => {
+const getAll = async (_req, res) => {
   const products = await productsModel.getAll();
 
   return res.status(200).json(products);
 };
 
+const createProduct = async (_req, res) => {
+  // const createProduct = await productsModel.createProduct();
+
+  return res.status(201).json({ message: 'Product created successfully' });
+};
+
 module.exports = {
   getAll,
+  createProduct,
 };
